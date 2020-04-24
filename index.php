2acf7c5e33d5f9201a8dcb25a2cd40df2fa890fc
Raihan Firmansyah <rehanfirmansyahr@gmail.com>
2020-04-24 22:43:51 +0700

    wek dewe

--- index.php
+++ index.php
@@ -6,27 +6,39 @@ include 'login/cek-login.php';
 
 <head>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
-    <style>
-        header {
-            width: 100%;
-            background-color: #166a94;
-            padding: 10px;
-            padding-left: 5%;
-            font-family: 'Montserrat', sans-serif;
-        }
-
-        body {
-            margin: 0;
-            padding: 0;
-        }
-    </style>
+    <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Data Penyabaran Covid-19</title>
 </head>
 
 <body>
-    <header>Data penyebaran virus covid-19</header>
+    <header>
+        <h3>Data penyebaran virus covid-19</h3>
+        <div class="hm">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </header>
+    <div class="bungkus">
+        <div class="sidebar">
+            <ul>
+                <li>lorem</li>
+                <li>lorem</li>
+                <li>lorem</li>
+                <li>lorem</li>
+                <li>lorem</li>
+                <li>lorem</li>
+                <li>lorem</li>
+                <li>lorem</li>
+                <li>lorem</li>
+            </ul>
+        </div>
+        <div class="content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit accusantium magnam, earum ut iste dolorem voluptate, est adipisci quia, quas blanditiis! Labore hic illum, repellendus est quaerat similique iure quisquam?</p>
+        </div>
+    </div>
 </body>
 
 </html>
